64da1fd01a756f0f0f9f4376f790e63b2e3d26b4
Brundeen <anonymousbrandon@yahoo.com>
2016-12-30 16:57:27 -0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2847,7 +2847,7 @@
 
             pingCommand: {
                 command: 'ping',
-                rank: 'user',
+                rank: 'bouncer',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
