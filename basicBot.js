e151fb25edcb3f135fcfd8d2899cdaaeb44ccb0b
Brundeen <anonymousbrandon@yahoo.com>
2016-12-30 00:47:52 -0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2490,7 +2490,7 @@
                         if (msg.length <= cmd.length + 1) return API.sendChat(subChat(basicBot.chat.currentlang, {language: basicBot.settings.language}));
                         var argument = msg.substring(cmd.length + 1);
 
-                        $.get("https://rawgit.com/basicBot/source/master/lang/langIndex.json", function (json) {
+                        $.get("https://raw.githubusercontent.com/Brundeen/source/master/lang/langIndex.json", function (json) {
                             var langIndex = json;
                             var link = langIndex[argument.toLowerCase()];
                             if (typeof link === "undefined") {
