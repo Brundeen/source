853b19d639138ce46b745cdb33a88e467827fe71
Brundeen <anonymousbrandon@yahoo.com>
2016-12-31 17:02:24 -0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2961,6 +2961,19 @@
                     }
                 }
             },
+            
+            vioreCommand: {
+                command: 'viore',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.viore)
+                    }
+                }
+            },
 
             //refreshCommand: {
                 //command: 'refresh',
