a00f14b026182e8f1b3ea568a2d315ee2a670e9f
Brundeen <anonymousbrandon@yahoo.com>
2017-01-02 11:15:24 -0500

    Update

--- basicBot.js
+++ basicBot.js
@@ -1284,13 +1284,13 @@
                 basicBot.room.roomstats.chatmessages++;
             },
             spam: [
-                'hueh', 'hu3', 'brbr', 'heu', 'kkkk', 'spoder', 'mafia', 'zuera', 'zueira',
+                'hueh', 'hu3', 'anime', 'brbr', 'heu', 'kkkk', 'spoder', 'mafia', 'zuera', 'zueira',
                 'zueria', 'aehoo', 'aheu', 'alguem', 'algum', 'brazil', 'zoeira', 'fuckadmins', 'affff', 'vaisefoder', 'huenaarea',
                 'hitler', 'ashua', 'ahsu', 'ashau', 'lulz', 'huehue', 'hue', 'huehuehue', 'merda', 'pqp', 'puta', 'mulher', 'pula', 'retarda', 'caralho', 'filha', 'ppk',
                 'gringo', 'fuder', 'foder', 'hua', 'ahue', 'modafuka', 'modafoka', 'mudafuka', 'mudafoka', 'ooooooooooooooo', 'foda'
             ],
             curses: [
-                'motherfucker', 'modafocka'
+                'motherfucker', 'nigger'
             ]
         },
         connectAPI: function () {
