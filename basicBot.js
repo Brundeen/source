0d79d199aa8720b28bcb866926d3115bb317d044
Brundeen <anonymousbrandon@yahoo.com>
2017-02-23 16:35:53 -0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3078,15 +3078,15 @@
                 }
             },
 		
-	    dcscapeCommand: {
-                command: 'dcscape',
+	    dcCommand: {
+                command: 'dc',
                 rank: 'user',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat(basicBot.chat.dcscape)
+                        API.sendChat(basicBot.chat.dc)
                     }
                 }
             },
