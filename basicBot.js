7d712d35eb6aeb032feddfc1fb171920d390cf69
Brundeen <anonymousbrandon@yahoo.com>
2017-06-16 00:44:24 -0400

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3137,7 +3137,20 @@
                 }
             },
 		
-	   settingsCommand: {
+	    cabbageCommand: {
+                command: 'cabbage',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.cabbage)
+                    }
+                }
+            },
+	  
+	    settingsCommand: {
                 command: 'settings',
                 rank: 'user',
                 type: 'exact',
